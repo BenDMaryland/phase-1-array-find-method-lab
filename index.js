e1d54bbf76751946a5ba621383f4ad73e8511c1b
Benjamin <ben.d.maryland@gmail.com>
2021-09-22 11:20:16 -0400

    commit tree 2b9d3c24b44b681f079dbfd0bea65e3cba4dbdb4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+  let y 
+function superbowlWin(record) {
+ 
+ y = record.find( ({ result }) => result === 'W' )
+if  (y != undefined){
+    return y.year
+} else
+return y
+}
 
+console.log(superbowlWin(record))
